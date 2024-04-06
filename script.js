6261555040e68d261161d29f37f1c36b4d7cd964
Ernest <ernestchidakwa785@gmail.com>
2024-04-05 22:58:25 -0600

    new content

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
-function darkMode() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-}
+let projects = [
+  {
+    website: "Weather",
+    link: "",
+    languages: ["React", "Javascript",]
+  },
+  {
+    website: "Larby",
+    link: "",
+    languages: ["HTML", "CSS", "Javascript",]
+  },
+  {
+
+  },
+  {
+
+  },
+  {
+
+  }
+]
+
+
+let projectHTML = ""
+
+projects.forEach((project) => {
+  projectHTML += `
+  <div class="project-div">
+    <h3>
+      ${project.website}
+    </h3>
+    <p>
+      
+    </p>
+  </div>
+  `
+})
+
+const root = document.querySelector("#projects").innerHTML = projectHTML;
